45375283897b1b4e0fa7cc9a8727b5435121c9e3
Biplo12 <robertbiplosek@gmail.com>
2022-11-05 13:49:03 +0100

    FIX: lint error

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from './Partials/Navbar';
 import TopBar from './Partials/TopBar';
 const Header: React.FC = (): JSX.Element => {
   return (
-    <div className="w-full">
+    <div className="w-full z-50">
       <TopBar />
       <Navbar />
     </div>
