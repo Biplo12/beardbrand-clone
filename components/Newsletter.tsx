8dfd986858196b84f622c9ee3187bfb1e0c057eb
Biplo12 <robertbiplosek@gmail.com>
2022-11-06 20:16:51 +0100

    ADD: newsletter

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -4,8 +4,8 @@ const Newsletter: React.FC = (): JSX.Element => {
     <div className="w-full min-h-[40vh] flex bg-alabaster text-charleston-green flex-col gap-5 justify-center items-center text-center p-5 mxsm:h-auto">
       <h1 className="font-bold font-pano text-2xl">GROW YOUR MIND</h1>
       <p className="max-w-[700px]">
-        Join the best newsletter that you never knew you needed. You'll get 3
-        emails per week containing grooming tips, new blog posts, product
+        Join the best newsletter that you never knew you needed. You&apos;ll get
+        3 emails per week containing grooming tips, new blog posts, product
         launches, giveaways, and motivation to help you Keep on Growing.
       </p>
       <div className="flex w-full justify-center items-center mxxsm:flex-col mxxsm:gap-5">
