b8999f0495aa78d55620c80787c35afcb047a2ea
Biplo12 <robertbiplosek@gmail.com>
2022-12-12 19:13:20 +0100

    ADD: slider-observer

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ImageSide from './Partials/ImageSide';
+import SplitSlider from './Partials/SplitSlider';
 const Hero: React.FC = (): JSX.Element => {
   return (
     <div className="flex justify-between items-center w-full bg-charleston-green mxlg:bg-transparent mxlg:justify-center relative">
-      <div className={`flex flex-col w-full right-0 h-full mxlg:w-auto`}>
-        <ImageSide />
+      <div className="flex flex-col w-full right-0 h-full">
+        <SplitSlider />
       </div>
     </div>
   );
--- components/Hero/Partials/SplitSlider.tsx
+++ components/Hero/Partials/SplitSlider.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import TextSide from './TextSide';
 
-const ImageSide: React.FC = (): JSX.Element => {
+const Split: React.FC = (): JSX.Element => {
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        console.log(entry);
+
+        if (entry.isIntersecting) {
+          //do your actions here
+          console.log('It works!');
+        }
+      },
+      {
+        root: null,
+        rootMargin: '100px',
+        threshold: 0.1,
+      }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+  }, [ref]);
   return (
     <>
       <div className="relative">
@@ -15,7 +37,10 @@ const ImageSide: React.FC = (): JSX.Element => {
         </div>
       </div>
       <div className="relative">
-        <div className="flex items-center justify-start w-full h-screen bg-fixed bg-top-4 bg-cover hero-img-2 brightness-50" />
+        <div
+          className="flex items-center justify-start w-full h-screen bg-fixed bg-top-4 bg-cover hero-img-2 brightness-50"
+          ref={ref}
+        />
         <div className="flex h-full absolute top-0 left-0">
           <TextSide
             title="COMING BLACK FRIDAY"
@@ -27,4 +52,4 @@ const ImageSide: React.FC = (): JSX.Element => {
     </>
   );
 };
-export default ImageSide;
+export default Split;
